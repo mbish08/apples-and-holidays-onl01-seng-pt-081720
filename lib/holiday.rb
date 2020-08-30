0b7853b3da0715ed426bdeaff409eede2b8a9814
Mikel Bishop <mdrew8@gmail.com>
2020-08-30 17:43:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,9 +63,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each do |season, holiday|
-  holiday.each do |holiday_name, supplies|
-    puts "#{holiday_name}: #{supplies}"
+holiday_hash.each do |season, holiday, supplies|
+    puts "#{season}: #{holiday_name}: #{supplies}"
 end
 end
 end
