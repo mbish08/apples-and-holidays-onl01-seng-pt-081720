d215c8f12b58c82a2ebb4e4b064567c3f7f96752
Mikel Bishop <mdrew8@gmail.com>
2020-08-30 17:17:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,9 +46,9 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # remember to return the updated hash
 holiday_hash[:spring].each do |holiday, decorations|
   decorations << "Table Cloth"
-  binding.pry 
 end 
 holiday_hash
+binding.pry
 end
 
 def all_winter_holiday_supplies(holiday_hash)
